a4ef4bb98e84270c71416337299868ad26b4228a
Mohamed <mohamed.sheik98@outlook.com>
2023-03-14 02:24:28 +0000

    JWT authentication

--- server/moviewer/settings.py
+++ server/moviewer/settings.py
@@ -84,6 +84,18 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'users.authentication.JWTAuthentication',
+    ],
+}
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
